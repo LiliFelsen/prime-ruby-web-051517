c10070f18641b50b5fb0091bc8b05566eb0d6d4d
Aurelie Felsen <aurelie.felsen@gmail.com>
2017-05-08 04:00:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number == 0 || number == 1
+    return false
+  end
+
+  i = 2
+  max = number/i
+  while i < max
+    if (number % i) == 0
+      return false
+    end
+    i +=1
+    max = number/i
+  end
+  return true
+end
